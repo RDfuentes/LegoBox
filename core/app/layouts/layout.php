9341223c5f2d14363d4c72e0a71f7b6e9639a642
Ronald David Fuentes <ronaldfuentes.newsoft@gmail.com>
2023-11-04 13:52:03 -0600

    frature / integración template skote
    - se integra el template de skote en todo el proyecto
    
    - se integra el login de skote funcional

--- core/app/layouts/layout.php
+++ core/app/layouts/layout.php
@@ -1,130 +1,63 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="es">
 
+<!-- encabezado -->
+
 <head>
-  <meta charset="utf-8">
-  <title>NEWSOFT</title>
-  <link rel="icon" type="image/png" sizes="16x16" href="img/legobox.png">
-  <link rel="stylesheet" href="res/font-awesome/css/font-awesome.min.css">
-  <link href="res/bootstrap/css/bootstrap.css" rel="stylesheet">
-  <script src="res/js/jquery.min.js"></script>
+	<!-- utf-8 -->
+	<meta charset="utf-8" />
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<title>LEGOBOX</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- Responsive -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- FondoTemplate -->
+	<style>
+		body {
+			background-image: url('assets/images/fondoinicial.png');
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+			background-size: cover;
+		}
+	</style>
 
-  <!-- Script para mostrar contraseña -->
-  <script type="text/javascript">
-    function mostrarPassword() {
-      var user = document.getElementById("thepassworduser");
-      if (user.type == "password") {
-        user.type = "text";
-        $('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
-      } else {
-        user.type = "password";
-        $('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
-      }
-    }
-
-    $(document).ready(function() {
-      $('#ShowPassword').click(function() {
-        $('#Password').attr('type', $(this).is(':checked') ? 'text' : 'password');
-      });
-    });
-  </script>
-
-  <!-- Script para DataTable -->
-  <script src="res/js/cdn.datatables.net_1.13.5_js_jquery.dataTables.min.js"></script>
-
-  <!-- Style para DataTable -->
-  <link rel="stylesheet" type="text/css" href="res/css/cdn.datatables.net_1.13.5_css_jquery.dataTables.min.css">
-
+	<!-- CSS -->
+	<?php
+	include('assets/css/styles.blade.php');
+	?>
 </head>
+<!-- end encabezado -->
 
-<body>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
+<body data-layout="horizontal">
 
-        <nav class="navbar navbar-default header">
+	<!-- Page -->
+	<div id="layout-wrapper">
 
-          <a class="navbar-brand">
-            <img src="img/legobox.png" alt="" width="30">
-          </a>
+		<header>
+			<?php
+			include('partes/header.php');
+			?>
+		</header>
 
-          <?php if (isset($_SESSION["admin_id"])) : ?>
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-            </div>
-          <?php endif; ?>
+		<div class="main-content">
+			<div class="page-content">
+				<?php
+				View::load("index");
+				?>
+			</div>
+		</div>
 
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-              <?php if (isset($_SESSION["admin_id"])) : ?>
-                <ul class="nav navbar-nav" style="text-align:center">
-                  <li class="primary nav-1">
-                    <a href="./?view=welcome">🏚️ Inicio</a>
-                  </li>
-                  <li class="primary nav-1">
-                    <a href="./?action=admin&adm=logout">❌ Cerrar sesion</a>
-                  </li>
-                </ul>
-              <?php endif; ?>
-            </ul>
-          </div>
+		<footer>
+			<?php
+			include('partes/footer.php');
+			?>
+		</footer>
 
-        </nav>
+	</div>
+	<!-- end Page -->
 
-        <div class="content" style="margin-top:70px;">
-          <?php
-          View::load("index");
-          ?>
-        </div>
-
-        <div class="text-center footer">
-          <h6 class="alert" style="background-color: rgba(255,255,255,1); color:#144676">
-            <strong>SOFTWARE DEVELOPMENT<a href="#" target="_blank" style="color:#144676"><br>NEWSOFT</a> &copy; 2023</strong>
-          </h6>
-        </div>
-
-      </div>
-    </div>
-  </div>
+	<!-- JAVASCRIPT -->
+	<?php
+	include('assets/js/scripts.blade.php');
+	?>
 </body>
-
-<script src="res/bootstrap/js/bootstrap.min.js"></script>
-
-<style>
-  .footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    background-color: rgba(255, 255, 255, 1);
-    color: #2c6838;
-    text-align: center;
-  }
-
-  .content {
-    margin-bottom: 100px;
-    /* Ajusta este valor según sea necesario */
-  }
-</style>
-
-
-<style>
-  .header {
-    width: 100%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    background-color: #ffffff;
-    z-index: 9999;
-  }
-</style>
-
-</html>
---
+++ core/app/layouts/partes/header.php
@@ -0,0 +1,26 @@
+<header id="page-topbar">
+    <div class="navbar-header">
+
+        <div class="navbar-brand-box">
+            <a href="#" class="logo logo-dark">
+                <span class="logo-sm">
+                    <img src="assets/images/logo.png" alt="" height="22">
+                </span>
+                <span class="logo-lg">
+                    <img src="assets/images/logo-light.png" alt="" height="29">
+                </span>
+            </a>
+        </div>
+
+        <div>
+            <a href="./?view=welcome">🏚️ Inicio</a>
+            <a href="./?action=admin&adm=logout">❌ Cerrar sesion</a>
+            <div class="dropdown d-none d-lg-inline-block ms-1">
+                <button type="button" class="btn header-item noti-icon waves-effect" data-bs-toggle="fullscreen">
+                    <i class="bx bx-fullscreen"></i>
+                </button>
+            </div>
+        </div>
+
+    </div>
+</header>
